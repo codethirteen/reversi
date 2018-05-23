f8520fb611f7021ff6b6856e7bda96d425c10956
Paul <admin@codethirteen.com>
2018-05-22 20:25:55 -0500

    part 9 completed

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+  /*eslint-env jquery, browser*/
 function getURLParameters(urlParams) {
   var pageURL = window.location.search.substring(1);
   var pageURLVariables = pageURL.split('&');
@@ -11,15 +12,56 @@ function getURLParameters(urlParams) {
 }
 
 var username = getURLParameters('username');
-if (typeof username === typeof undefined || !username) {
-  username = 'Anonymous_'+Math.random();
+if ('undefined' == typeof username || !username) {
+  username = 'Anonymous_' + Math.random();
 }
 
-$('#messages').append('<h4>'+getURLParameters('username')+'</h4>');
 
-// connect to socket server 
-var socket = socket.on.connect();
+
+var chat_room = 'One_Room';
+
+/* Connect to the socket server */
+
+var socket = io.connect();
 
 socket.on('log', function (array) {
   console.log.apply(console, array);
+});
+
+socket.on('join_room_response', function (payload) {
+  console.log('here');
+  if (payload.result == 'fail') {
+    alert(payload.message);
+    return;
+  }
+  $('#messages').append('<p>New user joined the room: ' + payload.username + '</p>');
+});
+
+
+socket.on('send_message_response', function (payload) {
+  console.log('here');
+  if (payload.result == 'fail') {
+    alert(payload.message);
+    return;
+  }
+  $('#messages').append('<p><b>' + payload.username + ' says:</b> ' + payload.message + '</p>');
+});
+
+
+function send_message() {
+  var payload = {};
+  payload.room = chat_room;
+  payload.username = username;
+  payload.message = $('#send_message_holder').val();
+  console.log('*** Client Log Message: \'send_message\' payload: ' + JSON.stringify(payload));
+  socket.emit('send_message', payload);
+}
+
+$(function () {
+  var payload = {};
+  payload.room = chat_room;
+  payload.username = username;
+
+  console.log('*** Client Log Message: \'join_room\ payload: ' + JSON.stringify(payload));
+  socket.emit('join_room', payload);
 });
--- server.js
+++ server.js
@@ -1,35 +1,47 @@
-/*jslint node: true */
-// SET UP THE STATIC FILE SERVER
-// include static file web server library
+/*********************************************/
+/*      Set up the static file server        */
+/* Include the static file webserver library */
 var static = require('node-static');
-// include the http server library
+
+/* Include the http server library */
 var http = require('http');
-// assume running on Heroku
+
+/* Assume that we are running on Heroku */
 var port = process.env.PORT;
 var directory = __dirname + '/public';
-// If not on Heroku, change port + dir info
+
+/* If we aren't on Heroku, then we need to readjust the port and directory
+ * information and we know that because port won't be set */
 if (typeof port == 'undefined' || !port) {
   directory = './public';
   port = 8080;
 }
-// set up static web server
-var file = new static.Server(directory);
-// construct HTTP server that pulls from file server
-var app = http.createServer(function (request, response) {
-  request.addListener('end', function () {
-    file.serve(request, response);
-  }).resume();
-}).listen(port);
-// log message so we know everything is a-okay
-console.log('The server is running');
 
-// SET UP THE WEB SOCKET SERVER
+/* Set up a static web-server that will deliver files from the filesystem */
+var file = new static.Server(directory);
+
+/* Construct an http server that get files from the file server */
+var app = http.createServer(
+  function (request, response) {
+    request.addListener('end',
+      function () {
+        file.serve(request, response);
+      }
+    ).resume();
+  }
+).listen(port);
+
+console.log('The Server is running');
+
+/**********************************************/
+/*      Set up the web socket file server     */
+
 var io = require('socket.io').listen(app);
 
-io.on('connection', function (socket) {
-  
+io.sockets.on('connection', function (socket) {
+
   function log() {
-    var array = ['*** Server log message: '];
+    var array = ['*** Server Log Message: '];
     for (var i = 0; i < arguments.length; i++) {
       array.push(arguments[i]);
       console.log(arguments[i]);
@@ -37,10 +49,156 @@ io.on('connection', function (socket) {
     socket.emit('log', array);
     socket.broadcast.emit('log', array);
   }
-  log('A website connected to the server');
+  log('A web site connected to the server');
 
   socket.on('disconnect', function (socket) {
-    log('A website disconnected from the server');
+    log('A web site disconnected from the server');
+  });
+
+  /* join_room command */
+  /* payload: 
+  {
+  'room'		: room to,
+  'username' 	: username of person joining
+  }
+  join_room_response: {
+  'result'	: 'success',
+  'room' 		: room joined,
+  'username' 	: username that joined,
+  'membership': number of people in the room including the new one
+  }
+  or 
+  {
+  'result': 'fail',
+  'message' : failure message
+  }
+  */
+
+  socket.on('join_room', function (payload) {
+    log('server received a command', 'join_room', payload);
+    if (('undefined' === typeof payload) || !payload) {
+      var error_message = 'join_room had no payload, command aborted';
+      log(error_message);
+      socket.emit('join_room_response', {
+        result: 'fail',
+        message: error_message
+      });
+      return;
+    }
+
+    var room = payload.room;
+    if (('undefined' === typeof room) || !room) {
+      var error_message = 'join_room didn\'t specify a room, command aborted';
+      log(error_message);
+      socket.emit('join_room_response', {
+        result: 'fail',
+        message: error_message
+      });
+      return;
+    }
+
+    var username = payload.username;
+    if (('undefined' === typeof username) || !username) {
+      var error_message = 'join_room didn\'t specify a username, command aborted';
+      log(error_message);
+      socket.emit('join_room_response', {
+        result: 'fail',
+        message: error_message
+      });
+      return;
+    }
+    socket.join(room);
+
+    var roomObject = io.sockets.adapter.rooms[room];
+    if (('undefined' === typeof roomObject) || !roomObject) {
+      var error_message = 'join_room couldn\'t create a room, command aborted';
+      log(error_message);
+      socket.emit('join_room_response', {
+        result: 'fail',
+        message: error_message
+      });
+      return;
+    }
+    var numClients = roomObject.length;
+    var success_data = {
+      result: 'success',
+      room: room,
+      username: username,
+      membership: (numClients + 1)
+    };
+    io.sockets.in(room).emit('join_room_response', success_data);
+    log('Room ' + room + ' was just joined by ' + username);
+  });
+
+  /* send_message command */
+  /* payload: 
+  {
+
+  'room'		: room to join,
+  'username' 	: username of person sending the message,
+  'message' 	: the message to send
+  }
+  send_message_response: {
+  'result'	: 'success',
+  'username' 	: username of the person that spoke,
+  'message' 	: the message spoken
+  }
+  or {
+  'result'	:'fail',
+  'message' 	: failure message
+  }
+  */
+  socket.on('send_message', function (payload) {
+    log('server received a command', 'send_message', payload);
+    if (('undefined' === typeof payload) || !payload) {
+      var error_message = 'send_message had no payload, command aborted';
+      log(error_message);
+      socket.emit('send_message_response', {
+        result: 'fail',
+        message: error_message
+      });
+      return;
+    }
+
+    var room = payload.room;
+    if (('undefined' === typeof room) || !room) {
+      var error_message = 'send_message didn\'t specify a room, command aborted';
+      log(error_message);
+      socket.emit('send_message_response', {
+        result: 'fail',
+        message: error_message
+      });
+      return;
+    }
+
+    var username = payload.username;
+    if (('undefined' === typeof username) || !username) {
+      var error_message = 'send_message didn\'t specify a username, command aborted';
+      log(error_message);
+      socket.emit('send_message_response', {
+        result: 'fail',
+        message: error_message
+      });
+      return;
+    }
+
+    var message = payload.message;
+    if (('undefined' === typeof message) || !message) {
+      var error_message = 'send_message didn\'t specify a username, command aborted';
+      log(error_message);
+      socket.emit('send_message_response', {
+        result: 'fail',
+        message: error_message
+      });
+      return;
+    }
+
+    var success_data = {
+      result: 'Success',
+      username: username,
+      message: message
+    };
+    io.sockets.in(room).emit('send_message_response', success_data);
+    log('Message sent to room ' + room + ' by ' + username);
   });
 });
-          
